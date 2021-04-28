f16625986e66d526abf87a36004a4425ba382d9b
kinggora <61868949+kinggora@users.noreply.github.com>
2021-04-29 04:28:14 +0900

    order db time 형식 수정, order sheet 레이아웃 수정

--- app/src/main/java/com/example/mobileproject_mellivora_capensis/DBHelperTest.java
+++ app/src/main/java/com/example/mobileproject_mellivora_capensis/DBHelperTest.java
@@ -16,9 +16,9 @@ public class DBHelperTest extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db){
         sql = "CREATE TABLE test (_id INTEGER PRIMARY KEY AUTOINCREMENT,"+ " type TEXT, menus TEXT, request TEXT, " + "time TEXT);";
         db.execSQL(sql);
-        db.execSQL("INSERT INTO TEST VALUES(NULL, '포장', '메뉴1, 메뉴2, 메뉴3', '요청사항1', '01:00 PM');");
-        db.execSQL("INSERT INTO TEST VALUES(NULL, '매장', '메뉴1, 메뉴2', ' ', '01:20 PM');");
-        db.execSQL("INSERT INTO TEST VALUES(NULL, '포장', '메뉴1, 메뉴2, 메뉴3, 메뉴4', '요청사항2', '01:40 PM');");
+        db.execSQL("INSERT INTO TEST VALUES(NULL, '포장', '메뉴1, 메뉴2, 메뉴3', '요청사항1', '2021년 1월 1일 오후 01:00');");
+        db.execSQL("INSERT INTO TEST VALUES(NULL, '매장', '메뉴1, 메뉴2', ' ', '2021년 1월 1일 오후 01:20');");
+        db.execSQL("INSERT INTO TEST VALUES(NULL, '포장', '메뉴1, 메뉴2, 메뉴3, 메뉴4', '요청사항2', '2021년 1월 1일 오후 01:40');");
 
     }
 
